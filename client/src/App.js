bb06c113f90dfc5e47edfb7a27441df657b3d37b
adithyan mp <adithyanmp2806@gmail.com>
2024-09-16 21:26:56 +0530

    corrected

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import SignInPage from "./pages/SignUpPage/CreateAccount"
+import LoginPage from "./pages/gigLogin/LoginPage"
+import HomePage from "./pages/HomePage/HomePage"
+import GigPool from "./pages/gigPool/GigPool"
+import GigJournal from "./pages/gigJournal/GigJournal"
+import GigTracker from "./pages/gigTracker/GigTracker"
 
 function App() {
   return (
     <div className="App">
-      <SignInPage />
+      <GigPool />
     </div>
   );
 }
