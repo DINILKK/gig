baff213c136e5ddcf0489571f9b0e485fbe164c6
adithyan mp <adithyanmp2806@gmail.com>
2024-09-21 23:55:35 +0530

    db schema

---
+++ server/models/providerCollectiondb.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  company: {
+    type: String,
+    required: true
+  },
+  postedJobs: [{
+    type: String,
+    required: true
+  }],
+  industry: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create the model
+const provider = mongoose.model('provider', userSchema);
+
+module.exports = provider;
---
+++ server/models/seekerCollectionsdb.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const applicantSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  appliedJobs: [{
+    type: String,
+    required: true
+  }],
+  skills: [{
+    type: String,
+    required: true
+  }],
+  location: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create the model
+const seeker = mongoose.model('seeker', applicantSchema);
+
+module.exports = seeker;
