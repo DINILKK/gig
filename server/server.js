e5e2e732de9b7cfe4f2507225dc7fab727023b3a
DINILKK <dinilkk007@gmail.com>
2024-10-17 19:30:03 +0530

    temp2

--- server/server.js
+++ server/server.js
@@ -20,12 +20,9 @@ app.use(express.json());
 
 // Mount the router at /api
 app.use('/api', gigPoolRouter);
-<<<<<<< HEAD
 app.use('/api', seekerCreationRouter);
 app.use('/api', providerCreationRouter)
-=======
 app.use('/api',gigTrackerRouter);
->>>>>>> a7d846e1e57b0afee2583bc44aed6be0000e27e7
 
 // Connect to database
 connectDB();
